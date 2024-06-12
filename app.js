6b3047c190490aed1ff3d16c14ec4294bf3ae556
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-12 15:07:55 -0600

    init + quick styling

---
+++ app.js
@@ -0,0 +1,138 @@
+let bank = 100
+
+const players = [
+  {
+    name: "D'Marcus Williums",
+    teamNumber: 0,
+    emoji: '🏃‍♂️',
+    skill: 10
+  },
+  {
+    name: "Tyroil Smoochie-Wallace",
+    teamNumber: 0,
+    emoji: '🤾‍♂️',
+    skill: 30
+  },
+  {
+    name: "Jackmerius Tacktheratrix",
+    teamNumber: 0,
+    emoji: '🏇',
+    skill: 88
+  },
+  {
+    name: "Javaris Jamar Javarison-Lamar",
+    teamNumber: 0,
+    emoji: '🏌️‍♀️',
+    skill: 15
+  },
+  {
+    name: "D'Pez Poopsie",
+    teamNumber: 0,
+    emoji: '🏋️‍♂️',
+    skill: 77
+  },
+  {
+    name: "D'Jasper Probincrux III",
+    teamNumber: 0,
+    emoji: '🏌️‍♂️',
+    skill: 21
+  },
+  {
+    name: "Leoz Maxwell Jilliumz",
+    teamNumber: 0,
+    emoji: '🤾',
+    skill: 5
+  },
+  {
+    name: "Hingle McCringleberry",
+    teamNumber: 0,
+    emoji: '🏂',
+    skill: 99
+  },
+  {
+    name: "L'Carpetron Dookmarriot",
+    teamNumber: 0,
+    emoji: '🧘‍♀️',
+    skill: 50
+  },
+  {
+    name: "Xmus Jaxon Flaxon-Waxon",
+    teamNumber: 0,
+    emoji: '🚶‍♀️',
+    skill: 1
+  },
+  {
+    name: "Saggitariutt Jefferspin",
+    teamNumber: 0,
+    emoji: '🏋️‍♀️',
+    skill: 61
+  },
+  {
+    name: "Quatro Quatro",
+    teamNumber: 0,
+    emoji: '🤺',
+    skill: 34
+  },
+  {
+    name: "X-Wing @Aliciousness",
+    teamNumber: 0,
+    emoji: '🏄',
+    skill: 71
+  },
+  {
+    name: "Bisquiteen Trisket",
+    teamNumber: 0,
+    emoji: '🧜‍♂️',
+    skill: 76
+  },
+  {
+    name: "Scoish Velociraptor Maloish",
+    teamNumber: 0,
+    emoji: '🤸',
+    skill: 47
+  },
+  {
+    name: "Donkey Teeth",
+    teamNumber: 0,
+    emoji: '⛹️‍♀️',
+    skill: 23
+  },
+  {
+    name: "T.J. A.J. R.J. Backslashinfourth V",
+    teamNumber: 0,
+    emoji: '🕴️',
+    skill: 58
+  },
+  {
+    name: "Firstname Lastname",
+    teamNumber: 0,
+    emoji: '💃',
+    skill: 99
+  },
+  {
+    name: "Dan Smith",
+    teamNumber: 0,
+    emoji: '🧍‍♂️',
+    skill: 3
+  },
+  {
+    name: "Tiger",
+    teamNumber: 0,
+    emoji: '🐅',
+    skill: 100
+  },
+]
+
+function drawPlayers() {
+    //Need to show all of the players in the array on the page via emojis
+    let playersEmojis = ''
+    players.forEach((player) => playersEmojis += `<span class="fs-1>${player.emoji}</span>`)
+    const playersElem = document.getElementById("players")
+    playersElem.innerHTML = playersEmojis;
+    console.log('Did this work?', playersEmojis)
+}
+
+function userBalance() {
+    // Shows the user's current balance
+    
+}
